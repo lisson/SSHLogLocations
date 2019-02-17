a25b2534ce4d3be2ae91d8992b57a1ed726c0055
andy <hmm@hmm.com>
2019-02-16 20:15:38 -0800

    tracks the last 15 entries and returns them as json over HTTP

--- SSHLogParser.py
+++ SSHLogParser.py
@@ -5,6 +5,7 @@ import re
 import sys
 import io
 import operator
+from collections import deque
 
 class Entry(object):
     def __init__(self):
@@ -15,10 +16,10 @@ class Entry(object):
 
 
 class GeoIPManager(object):
-    def __init__(self, filep):
-        self.filepath = filep
+    def __init__(self, filepath):
+        self.filepath = filepath
 
-    def LoadFile(self):
+    def LoadDBFile(self):
         self.reader = geoip2.database.Reader(self.filepath)    
     
     def LookUp(self, ip):
@@ -32,39 +33,52 @@ class GeoIPManager(object):
     def close(self):
         self.reader.close()
 
-def parse(line):
-    return 0
-
-def parseFile(filename):
-    ip_pattern = re.compile(".*[\b\s]((?:\d{1,3}\.){3}\d{1,3}).*")
-    results = []
-    g = GeoIPManager("GeoLite2-City.mmdb")
-    g.LoadFile()
-
-    ips = []
-    f = open(filename)
-    for line in f:
-        match = ip_pattern.match(line)
+class Parser(object):
+    def __init__(self, databaseFilePath):
+        self.IPRegex = re.compile(".*[\b\s]((?:\d{1,3}\.){3}\d{1,3}).*")
+        self.GeoIP = GeoIPManager(databaseFilePath)
+        self.GeoIP.LoadDBFile()
+    def Parse(self,lines):
+        match = self.IPRegex.match(lines)
         if (match != None):
-            ips.append(match.group(1))
-    f.close()
-    ips = list(set(ips))
-    for ip in ips:
-        e = g.LookUp(ip)
-        e.line = line.strip()
-        results.append(e)
-    g.close()
-    return results
-def RoundEntry(entry):
-    entry.latitude = round(entry.latitude)
-    entry.longtitude = round(entry.longtitude)
-    return entry
+            ip = match.group(1)
+        else:
+            return None
+        e = self.GeoIP.LookUp(ip)
+        e.line = lines.strip()
+        return e
+
+    def ParseFile(self,filename):
+        results = []
+        #g = GeoIPManager("GeoLite2-City.mmdb")
+        ips = []
+        f = open(filename)
+        for line in f:
+            match = self.IPRegex.match(line)
+            if (match != None):
+                ips.append(match.group(1))
+        f.close()
+        ips = list(set(ips))
+        for ip in ips:
+            e = self.GeoIP.LookUp(ip)
+            e.line = line.strip()
+            results.append(e)
+        return results
+
+    def RoundEntry(self,entry):
+        entry.latitude = round(entry.latitude)
+        entry.longtitude = round(entry.longtitude)
+        return entry
+    
+    def CloseGeoIP(self):
+        self.GeoIP.close()
 
 if __name__ == '__main__':
     if len(sys.argv) < 2 :
         exit(1)
-    results = parseFile(sys.argv[1])
-    results = map(RoundEntry, results)
+    parser = Parser("GeoLite2-City.mmdb")    
+    results = parser.ParseFile(sys.argv[1])
+    results = map(parser.RoundEntry, results)
     s = sorted(results, key = lambda x: (x.latitude, x.longtitude))
     coordinates = set()
     results = []
--- server.py
+++ server.py
@@ -2,14 +2,50 @@ import re
 import sys
 import io
 from flask import Flask
+from collections import deque
+import os
+import time
+import json
+import threading
+
 import SSHLogParser
 
-
 app = Flask(__name__)
+LastEntries = deque([], maxlen=15)
+logParser = SSHLogParser.Parser("GeoLite2-City.mmdb")
+logFile = None
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/locations")
+def locations():
+    return json.dumps(list(LastEntries), default=lambda o: o.__dict__)
+
+def UpdateLogQueue():
+    f = open(logFile, mode='rb')
+    f.seek(-2, os.SEEK_END)
+    while(True):
+        line = f.readline().decode(encoding="utf8")
+        if (line == ""):
+            time.sleep(1)
+        else:
+            x = logParser.Parse(line)
+            if (x != None):
+                LastEntries.append(x)
+
+def StartUpdateLogQueueThread():
+    thread = threading.Thread(target=UpdateLogQueue, args=())
+    thread.daemon = True                            # Daemonize thread
+    thread.start()
+
+
 if __name__ == '__main__':
+    if len(sys.argv) < 2 :
+        exit(1)
+    if(os.path.isfile(sys.argv[1]) == False):
+        print("{sys.argv[1]} not found")
+    logFile = sys.argv[1]
+    StartUpdateLogQueueThread()
     app.run()
